d5e818d1043c896a86b30d5c25b6133fbf913056
Siri Høyås <siri@skjervold.me>
2022-01-04 10:17:37 +0100

    did question 1+2

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Question 1
+var age = 29;
+var isPandemicAnnoying = true; 
+var favouriteHoliday = "christmas";
+
+//Question 2
+var firstName = "Siri";
+var lastName = "Høyås";
+var fullName = firstName + " " + lastName; 
+console.log(fullName);
+
+//Question 3
+
+//Question 4
+
+//Question 5
