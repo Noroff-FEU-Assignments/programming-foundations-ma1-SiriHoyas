65559d7633cd8878836537c91f362bd46331d185
Siri Høyås <siri@skjervold.me>
2022-01-06 10:59:44 +0100

    Finished tasks

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 //Question 1
-var age = 29;
-var isPandemicAnnoying = true; 
-var favouriteHoliday = "christmas";
+let age = 29;
+const isPandemicAnnoying = true; 
+let favouriteHoliday = "christmas";
 
 //Question 2
-var firstName = "Siri";
-var lastName = "Høyås";
-var fullName = firstName + " " + lastName; 
+const firstName = "Siri";
+const lastName = "Høyås";
+const fullName = firstName + " " + lastName; 
 console.log(fullName);
 
 //Question 3
+const typeOfFrog = typeof "frog";
+console.log("The type of frog is " + typeOfFrog);
 
 //Question 4
+let orderHasShipped = false;
+if(orderHasShipped === true) {
+    console.log("The order has shipped!");
+}
+else {
+    console.log("The order did not ship.");
+}
 
 //Question 5
+for(let i = 7; i <= 13; i++) {
+    console.log(i);
+}
